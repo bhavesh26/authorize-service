2998424198ddadea30d8e504db09a1082606f677
Bhavesh Bhayal <bhaveshbhayal@Bhaveshs-MacBook-Air.local>
2026-01-31 23:48:31 +0530

    zod setup

---
+++ src/schemas/auth.schema.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+const email = z.string().trim().toLowerCase().email();
+const password = z.string().min(8).max(72);
+const role = z.string().trim().toLowerCase();
+const username = z.string().trim()
+
+export const signupSchema = z.object({
+  email,
+  password,
+  role,
+  username
+});
+
+export const loginSchema = z.object({
+  email,
+  password,
+});
+
+
+
+export type SignupInput = z.infer<typeof signupSchema>;
+export type LoginInput = z.infer<typeof loginSchema>;
